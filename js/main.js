cbdfb9ed22d9e76207ac760fe5602044f39e014f
Alex Beals <abeals@fb.com>
2023-07-31 20:27:27 -0400

    General approach, need to specialize tiffs b/c google won't display them (and this way I can resize)

--- js/main.js
+++ js/main.js
@@ -1,28 +1,59 @@
 function drop(evt) {
     evt.stopPropagation();
     evt.preventDefault();
-    var imageUrl = evt.dataTransfer.getData('URL');
+    // var imageUrl = evt.dataTransfer.getData('URL');
 
-    // Update form and image
-    $('#image').css('background-image', 'url(' + imageUrl + ')');
-    $('#image').removeClass('blank');
-    $('form input[name="url"]').val(imageUrl);
+  // Get the dropped file from the event data
+  const file = evt.dataTransfer.files[0];
+  console.log(file.type);
 
-    $('#chosen span').html(imageUrl);
+  // Check if the dropped file is an image
+  if (file && file.type.startsWith('image/')) {
+    // Create a FileReader object to read the file
+    const reader = new FileReader();
 
-    // Update all of the relevant images (and possible warning)
-    $.post("similar.php", { url: imageUrl })
-        .done(function (data) {
-            var similar = JSON.parse(data);
-            if (similar.status == "success") {
-                var n = "";
-                for (var url in similar.closest) {
-                    var thumbnail = url.substring(0, url.length - 4) + "t" + url.substring(url.length - 4);
-                    n += '<div class="image" style="background-image: url(' + thumbnail + ');" data-similarity="' + similar.closest[url] + '"></div>';
-                }
-                $('#similar').html(n);
-            }
-        });
+    // Set up the FileReader event when the image is loaded
+    reader.onload = (e) => {
+        console.log(e);
+      // Set the image source to the loaded data URL
+      document.getElementById('imageDisplay').src = e.target.result;
+
+    };
+
+    reader.onprogress = (event) => {
+        if (event.lengthComputable) {
+          const progress = (event.loaded / event.total) * 100;
+          console.log(progress);
+        //   progressBar.value = progress;
+        }
+      };
+
+    // Read the file as a data URL
+    reader.readAsDataURL(file);
+} else {
+    // TODO: Handle the error case
+}
+
+    // // Update form and image
+    // $('#image').css('background-image', 'url(' + imageUrl + ')');
+    // $('#image').removeClass('blank');
+    // $('form input[name="url"]').val(imageUrl);
+
+    // $('#chosen span').html(imageUrl);
+
+    // // Update all of the relevant images (and possible warning)
+    // $.post("similar.php", { url: imageUrl })
+    //     .done(function (data) {
+    //         var similar = JSON.parse(data);
+    //         if (similar.status == "success") {
+    //             var n = "";
+    //             for (var url in similar.closest) {
+    //                 var thumbnail = url.substring(0, url.length - 4) + "t" + url.substring(url.length - 4);
+    //                 n += '<div class="image" style="background-image: url(' + thumbnail + ');" data-similarity="' + similar.closest[url] + '"></div>';
+    //             }
+    //             $('#similar').html(n);
+    //         }
+    //     });
 }
 
 function verify(evt) {
